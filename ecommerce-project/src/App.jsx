a5081cbb7848352721701261307ed57154385022
Bharath14790 <bh147000@gmail.com>
2026-02-28 17:47:46 +0530

    update the link

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
   <Routes>
-    <Route path = "/" element={<Homepage />}></Route>
+    <Route index element={<Homepage />}></Route>
     <Route path = "checkout" element={ <Checkout/> }></Route>
   </Routes>
    
